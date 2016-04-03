841a29f245adec470d02b6d22c9acfcee1f26d7f
alvilsk <ttepatt@gmail.com>
2016-04-03 16:43:45 +0000

    Fix heading, add footer

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,7 +1,11 @@
 class ListingsController < ApplicationController
+    before_action :find_listing, only: [:show, :edit, :update, :destroy]
     
     def index
       @listings = Listing.all.order("created_at DESC")
+    end
+    
+    def show
     end
     
     def new
@@ -11,16 +15,35 @@ class ListingsController < ApplicationController
     def create
       @listing = Listing.new(listing_params)
       if @listing.save
-        redirect_to root_path
+        redirect_to listings_path
       else
         render 'new'
       end
+    end
+    
+    def edit
+      @listing = Listing.edit
+    end
+    
+    def update
+      if @listing.update(listing_params)
+        redirect_to listing_path(@listing)
+      else
+        render 'edit'
+      end
+    end
+    
+    def destroy
     end
     
     private
     
     def listing_params
       params.require(:listing).permit(:title, :description, :price)
+    end
+    
+    def find_listing
+      @listing = Listing.find(params[:id])
     end
     
 end
